8d174e9e40ea366f448208c0fc3b769ddbf0259a
Cathybiwott <biwottcathy@gmail.com>
2023-05-25 14:25:57 +0300

    first commit

--- app/src/main/java/com/example/recyclerviewsbonacci/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewsbonacci/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.recyclerviewsbonacci
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recyclerviewsbonacci.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        fibonacci()
+    }
+    fun fibonacci():List<Int>{
+        var numbers = mutableListOf<Int>()
+        numbers.add(0)
+        numbers.add(1)
+
+        for(i in 2 until 101){
+            numbers.add(numbers[i-1]+ numbers[i-2])
+        }
+        binding.rvNums.layoutManager = LinearLayoutManager(this)
+        val namesAdapter = NumbersRvAdapter(numbers)
+        binding.rvNums.adapter = namesAdapter
+        return numbers
     }
 }
--- app/src/main/java/com/example/recyclerviewsbonacci/NumbersRvAdapter.kt
+++ app/src/main/java/com/example/recyclerviewsbonacci/NumbersRvAdapter.kt
@@ -1,7 +1,27 @@
 package com.example.recyclerviewsbonacci
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class NumbersRvAdapter(var )
+class NumbersRvAdapter(var numbers:List<Int>):RecyclerView.Adapter<NumbersRvAdapter.NumbersViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumbersRvAdapter.NumbersViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.numbers_list_item,parent,false)
+        return NumbersViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: NumbersRvAdapter.NumbersViewHolder, position: Int) {
+        holder.tvNums.text = numbers[position].toString()
+
+    }
+
+    override fun getItemCount(): Int {
+        return numbers.size
+
+    }
+    class NumbersViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        var tvNums = itemView.findViewById<TextView>(R.id.tvNums)
+    }
+}
